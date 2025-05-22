bfe31dc4ac66d5d068fb105c432fd3df8cc2ecc5
CodeWith-Nikita <n04616182@gmail.com>
2025-05-23 00:15:09 +0530

    Added new HTML and Javascript files

---
+++ helloWorld.js
@@ -0,0 +1,6 @@
+console. log("Hello world!");
+console. log("Hello Nikita!");
+console. log("I love Javascript!");
+fullName = "Nikita";
+console.log(fullName);
+console. log("Cloning Javascript project to Github");
