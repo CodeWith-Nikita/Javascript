55fb4ce5b33fb7eede07b17d21ce50d7f00587e9
CodeWith-Nikita <n04616182@gmail.com>
2025-05-23 17:47:13 +0530

    Learned taking input from user from HTML for JS and type casting.

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-console. log("Hello world!");
-console. log("Hello Nikita!");
-console. log("I love Javascript!");
-fullName = "Nikita";
-console.log(fullName);
-console. log("Cloning Javascript project to Github");
+// console. log("Hello world!");
+// console. log("Hello Nikita!");
+// console. log("I love Javascript!");
+// fullName = "Nikita";
+// console.log(fullName);
+// console. log("Cloning Javascript project to Github");
+
+// // CONTENT for displaying on website
+
+// window.alert("This is an alert.");
+// document.getElementById("myh2-forjs").textContent = "This text is written by Javascript.";
+// let firstName = "Nikita";
+// let favouriteFood = "Pizza";
+// let email = "Nikita123@gmail.com";
+
+// console.log(typeof email);
+// console.log("Your name is ", firstName);
+// console.log("You like ", favouriteFood); 
+// console.log("Your Email is ", email);
+
+// let studentName = "Raghav";
+// let age = 23;
+
+// document.getElementById("p1").textContent = `Student name is ${studentName}`;
+// document.getElementById("p2").textContent = `Student age is ${age}`;
+
+// <---------------Taking username as input----------->
+
+// let username;
+// document.getElementById("submit").onclick = function(){
+//     username =document.getElementById("username").value;
+//     document.getElementById("hello").textContent = `Hello ${username}`;
+//     console.log(username);
+// }
+
+// <---------Type conversion------->
+
+let age = window.prompt("How old are you?");
+age = Number(age);
+age += 1;
+console.log(age, typeof age);
